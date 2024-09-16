79cc97b7ce8bacf7f2a021b8e6241244505c5a4f
Christian Eckardt <eckardt.christian@web.de>
2024-09-16 23:04:25 +0200

    start getting rid of jquery

--- js_schoolfight/ropegame.js
+++ js_schoolfight/ropegame.js
@@ -1,4 +1,11 @@
-const canvas = $("#canvas")[0]
+const box = document.getElementById("box")
+const energyChar3 = document.getElementById("energy-char3")
+const girlLeft = document.getElementById("girl-left")
+const girlRight = document.getElementById("girl-right")
+const clickOverlay = document.getElementById("click-overlay")
+const ropeSkips = document.getElementById("rope-skips")
+const titleSelect = document.getElementById("title-select")
+const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 
 let moveRopeDownwards = false
@@ -12,7 +19,7 @@ const failSound = new Audio("wrong.wav")
 const girlcry = new Audio("girlcry.mp3")
 const deathcry = new Audio("deathcry.wav")
 
-$("#click-overlay").click(() => jump())
+clickOverlay.addEventListener("click", jump)
 
 function moveRope() {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -33,59 +40,64 @@ function moveRope() {
     if (y >= 280 || y <= 80) {
         moveRopeDownwards = !moveRopeDownwards
         skips += .5
-        if (moveRopeDownwards) {
-            $(canvas).css({ "z-index": "1" })
-        } else { $(canvas).css({ "z-index": "3" }) } if (skips % 3 == 0) {
+        canvas.style.zIndex = moveRopeDownwards ? "1" : "3"
+        if (skips % 3 == 0) {
             clearInterval(ropeInterval); gamespeed *= 1.1; ropeInterval = setInterval(moveRope, 1000 / gamespeed)
-        } if (y >= 240) {
-            if (characterJumps && party[2].energy > 0) {
-                points += 1
-                confirmSound.currentTime = 0
-                confirmSound.play()
-                $("#rope-skips").html(`Skips: ${points}/20`)
+        } 
 
-                if (points == 20) {
-                    round += 1
-                    $("#girl-left").addClass("defeated")
-                    $("#girl-right").addClass("defeated")
-                    clearInterval(ropeInterval)
-                    girlcry.play()
-                    document.getElementById("title-select").style.display = "block"
-                    document.getElementById("title-select").innerHTML =
-                        `You won! <br><br><br><br> <button id="next-button">Next round</button>`
-                    $("canvas").css({ "display": "none" })
-                    $("#click-overlay").css({ "display": "none" })
-                }
-            } else {
-                party[2].energy -= 35
-                failSound.currentTime = 0
-                failSound.play()
-                $("#energy-char3").css({ "width": party[2].energy + "%" })
-                if (party[2].energy <= 0) {
-                    $("#box").addClass("defeated")
-                    setTimeout(() => {
-                        clearInterval(ropeInterval)
-                        deathcry.play()
-                        $("canvas").css({ "display": "none" })
-                        $("#click-overlay").css({ "display": "none" })
-                        $("#title-select").css({ "display": "block" })
-                        $("#title-select").html(
-                            `You lost...!? <br><br><br><br> <button id="refresh-button">Try again</button>`)
-                    }
-                        , 200)
-                }
-            }
-        }
+        if (y < 240) return
+        
+        (characterJumps && party[2].energy > 0) ? handleJumpedOverRope() : handleHitByRope() 
+
     }
 }
 
 function jump() {
     if (!characterJumps) {
         characterJumps = true
-        $("#box").css({ "top": "140px" })
+        box.style.top = "140px"
         setTimeout(() => {
             characterJumps = false
-            $("#box").css({ "top": "170px" })
+            box.style.top = "170px"
         }, 300)
+    }
+}
+
+function handleJumpedOverRope() {
+    points += 1
+    confirmSound.currentTime = 0
+    confirmSound.play()
+    ropeSkips.innerHTML = `Skips: ${points}/20`
+
+    if (points == 20) {
+        round += 1
+        girlLeft.classList.add("defeated")
+        girlRight.classList.add("defeated")
+        clearInterval(ropeInterval)
+        girlcry.play()
+        titleSelect.style.display = "block"
+        titleSelect.innerHTML =
+            `You won! <br><br><br><br> <button id="next-button">Next round</button>`
+        canvas.style.display = "none"
+        clickOverlay.style.display = "none"
+    }
+}
+
+function handleHitByRope() {
+    party[2].energy -= 35
+    failSound.currentTime = 0
+    failSound.play()
+    energyChar3.style.width = party[2].energy + "%"
+    if (party[2].energy <= 0) {
+        box.classList.add("defeated")
+        setTimeout(() => {
+            clearInterval(ropeInterval)
+            deathcry.play()
+            canvas.style.display = "none"
+            clickOverlay.style.display = "none"
+            titleSelect.style.display = "block"
+            titleSelect.innerHTML =
+                `You lost...!? <br><br><br><br> <button id="refresh-button">Try again</button>`
+        }, 200)
     }
 }
